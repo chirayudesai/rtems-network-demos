16a968e4ef32afc8e06234f8e8187e3107ae45bc
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-02-10 19:54:56 +0000

    Modified so warnings would be printed if no network driver was defined.

--- networkconfig.h
+++ networkconfig.h
@@ -12,7 +12,17 @@
 #ifndef _RTEMS_NETWORKCONFIG_H_
 #define _RTEMS_NETWORKCONFIG_H_
 
-#define RTEMS_USE_BOOTP
+#ifndef RTEMS_BSP_NETWORK_DRIVER_NAME
+#warning "RTEMS_BSP_NETWORK_DRIVER_NAME is not defined"
+#define RTEMS_BSP_NETWORK_DRIVER_NAME "no_network1"
+#endif
+
+#ifndef RTEMS_BSP_NETWORK_DRIVER_ATTACH
+#warning "RTEMS_BSP_NETWORK_DRIVER_ATTACH is not defined"
+#define RTEMS_BSP_NETWORK_DRIVER_ATTACH 0
+#endif
+
+/* #define RTEMS_USE_BOOTP */
 
 #include <bsp.h>
 
@@ -21,9 +31,11 @@
  * Ethernet address here.  If RTEMS_SET_ETHERNET_ADDRESS is not
  * defined the driver will choose an address.
  */
-/* #define RTEMS_SET_ETHERNET_ADDRESS */
+#define RTEMS_SET_ETHERNET_ADDRESS
 #if (defined (RTEMS_SET_ETHERNET_ADDRESS))
-static char ethernet_address[6] = { 0x08, 0x00, 0x3e, 0x12, 0x28, 0xb1 };
+/* static char ethernet_address[6] = { 0x08, 0x00, 0x3e, 0x12, 0x28, 0xb1 }; */
+static char ethernet_address[6] = { 0x00, 0x80, 0x7F, 0x22, 0x61, 0x77 };
+
 #endif
 
 /*
@@ -39,7 +51,7 @@ static struct rtems_bsdnet_ifconfig netdriver_config = {
 	NULL,				/* BOOTP supplies IP address */
 	NULL,				/* BOOTP supplies IP net mask */
 #else
-	"128.233.14.91",		/* IP address */
+	"192.168.1.100",		/* IP address */
 	"255.255.255.0",		/* IP net mask */
 #endif /* !RTEMS_USE_BOOTP */
 
@@ -68,11 +80,18 @@ struct rtems_bsdnet_config rtems_bsdnet_config = {
 	0,			/* Default mbuf cluster capacity */
 
 #if (!defined (RTEMS_USE_BOOTP))
+#if 0
 	"rgamon",		/* Host name */
 	"usask.ca",		/* Domain name */
 	"128.233.14.100",	/* Gateway */
 	"128.233.14.1",		/* Log host */
 	{"128.233.14.1" },	/* Name server(s) */
+#endif
+	"dy4",			/* Host name */
+	"oarcorp.com",		/* Domain name */
+	"192.168.1.2",   	/* Gateway */
+	"192.168.1.2", 		/* Log host */
+	{"192.168.1.2" },	/* Name server(s) */
 #endif /* !RTEMS_USE_BOOTP */
 
 };
@@ -81,8 +100,8 @@ struct rtems_bsdnet_config rtems_bsdnet_config = {
  * For TFTP test application
  */
 #if (!defined (RTEMS_USE_BOOTP))
-#define RTEMS_TFTP_TEST_HOST_NAME "128.233.14.1"
-#define RTEMS_TFTP_TEST_FILE_NAME "bootfiles/m68360/RGAmonitor"
+#define RTEMS_TFTP_TEST_HOST_NAME "192.168.1.2"
+#define RTEMS_TFTP_TEST_FILE_NAME "tftptest"
 #endif
 
 #endif /* _RTEMS_NETWORKCONFIG_H_ */
