12e60d3ac4cc4611e07d81a94256c579e65e91a5
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-09-25 16:17:01 +0000

    2007-09-25	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* networkconfig-icecube.h: Fix MAC address and typo.

--- networkconfig-icecube.h
+++ networkconfig-icecube.h
@@ -1,5 +1,5 @@
 /*
- * Network configuration for IceCub in RTEMS Lab
+ * Network configuration for IceCube in RTEMS Lab
  * 
  ************************************************************
  * EDIT THIS FILE TO REFLECT YOUR NETWORK CONFIGURATION     *
@@ -41,7 +41,7 @@
  */
 #define RTEMS_SET_ETHERNET_ADDRESS
 #if (defined (RTEMS_SET_ETHERNET_ADDRESS))
-  static char ethernet_address[6] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55 };
+  static char ethernet_address[6] = { 0x00, 0x04, 0x9F, 0x00, 0x5B, 0x21 };
 #endif
 
 #ifdef RTEMS_USE_LOOPBACK 
